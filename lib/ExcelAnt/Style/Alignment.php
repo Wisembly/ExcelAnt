8fdef48d8c0ffd307ee92f5cb2bb57597c057c57
Rémy GAZELOT <r.gazelot@gmail.com>
2013-05-27 12:15:54 +0200

    Add Alignment and its tests

--- lib/ExcelAnt/Style/Alignment.php
+++ lib/ExcelAnt/Style/Alignment.php
@@ -6,5 +6,82 @@ use ExcelAnt\Style\StyleInterface;
 
 class Alignment implements StyleInterface
 {
+    /* Horizontal alignment styles */
+    const HORIZONTAL_GENERAL                = 'general';
+    const HORIZONTAL_LEFT                   = 'left';
+    const HORIZONTAL_RIGHT                  = 'right';
+    const HORIZONTAL_CENTER                 = 'center';
+    const HORIZONTAL_CENTER_CONTINUOUS      = 'centerContinuous';
+    const HORIZONTAL_JUSTIFY                = 'justify';
 
+    /* Vertical alignment styles */
+    const VERTICAL_BOTTOM                   = 'bottom';
+    const VERTICAL_TOP                      = 'top';
+    const VERTICAL_CENTER                   = 'center';
+    const VERTICAL_JUSTIFY                  = 'justify';
+
+    private $horizontal;
+    private $vertical;
+
+    public function setVertical($alignment)
+    {
+        $this->checkVerticalParameter($alignment);
+        $this->vertical = $alignment;
+
+        return $this;
+    }
+
+    public function getVertical()
+    {
+        return $this->vertical;
+    }
+
+    public function setHorizontal($alignment)
+    {
+        $this->checkHorizontalParameter($alignment);
+        $this->horizontal = $alignment;
+
+        return $this;
+    }
+
+    public function getHorizontal()
+    {
+        return $this->horizontal;
+    }
+
+    public function getHorizontals()
+    {
+        return [
+            self::HORIZONTAL_GENERAL,
+            self::HORIZONTAL_LEFT,
+            self::HORIZONTAL_RIGHT,
+            self::HORIZONTAL_CENTER,
+            self::HORIZONTAL_CENTER_CONTINUOUS,
+            self::HORIZONTAL_JUSTIFY,
+        ];
+    }
+
+    public function getVerticals()
+    {
+        return [
+            self::VERTICAL_BOTTOM,
+            self::VERTICAL_TOP,
+            self::VERTICAL_CENTER,
+            self::VERTICAL_JUSTIFY,
+        ];
+    }
+
+    private function checkHorizontalParameter($alignment)
+    {
+        if (!in_array($alignment, $this->getHorizontals())) {
+            throw new \InvalidArgumentException("The parameter must belong to the Horizontal parameters list");
+        }
+    }
+
+    private function checkVerticalParameter($alignment)
+    {
+        if (!in_array($alignment, $this->getVerticals())) {
+            throw new \InvalidArgumentException("The parameter must belong to the Vertical parameters list");
+        }
+    }
 }
--- test/ExcelAnt/Style/AlignmentTest.php
+++ test/ExcelAnt/Style/AlignmentTest.php
@@ -4,8 +4,49 @@ namespace ExcelAnt\Style;
 
 class AlignmentTest extends \PHPUnit_Framework_TestCase
 {
-    public function testTmp()
+    /**
+     * @dataProvider getWrongParameters
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetVerticalWithWrongParameter($param)
     {
-        $this->markTestIncomplete();
+        $alignment = new Alignment();
+        $alignment->setVertical($param);
+    }
+
+    public function testSetAndGetVertical()
+    {
+        $alignment = new Alignment();
+        $alignment->setVertical('bottom');
+
+        $this->assertEquals('bottom', $alignment->getVertical());
+    }
+
+    /**
+     * @dataProvider getWrongParameters
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetHorizontalWithWrongParameter($param)
+    {
+        $alignment = new Alignment();
+        $alignment->setHorizontal($param);
+    }
+
+    public function testSetAndGetHorizontal()
+    {
+        $alignment = new Alignment();
+        $alignment->setHorizontal('left');
+
+        $this->assertEquals('left', $alignment->getHorizontal());
+    }
+
+    public function getWrongParameters()
+    {
+        return [
+            ['foo'],
+            [''],
+            [null],
+            ['@&'],
+        ];
     }
 }
