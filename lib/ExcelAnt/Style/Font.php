84f3262a1b7025dd6732c1d5c7ffa96fd92972e0
Rémy GAZELOT <r.gazelot@gmail.com>
2013-05-27 15:15:12 +0200

    Add Font style

--- lib/ExcelAnt/Style/Font.php
+++ lib/ExcelAnt/Style/Font.php
@@ -6,5 +6,126 @@ use ExcelAnt\Style\StyleInterface;
 
 class Font implements StyleInterface
 {
+    /* Underline types */
+    const UNDERLINE_NONE             = 'none';
+    const UNDERLINE_DOUBLE           = 'double';
+    const UNDERLINE_DOUBLEACCOUNTING = 'doubleAccounting';
+    const UNDERLINE_SINGLE           = 'single';
+    const UNDERLINE_SINGLEACCOUNTING = 'singleAccounting';
 
+    private $name;
+    private $size;
+    private $bold;
+    private $italic;
+    private $color;
+    private $underline;
+
+    public function __construct()
+    {
+        $this->name = 'Calibri';
+        $this->size = 11;
+        $this->bold = false;
+        $this->italic = false;
+        $this->color = '000000';
+        $this->underline = self::UNDERLINE_NONE;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setSize($size)
+    {
+        if (!is_numeric($size)) {
+            throw new \InvalidArgumentException("The parameter must be a numeric value");
+        }
+
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    public function setBold($bold)
+    {
+        if (!is_bool($bold)) {
+            throw new \InvalidArgumentException("The parameter must be a boolean value");
+        }
+
+        $this->bold = $bold;
+
+        return $this;
+    }
+
+    public function isBold()
+    {
+        return $this->bold;
+    }
+
+    public function setItalic($italic)
+    {
+        if (!is_bool($italic)) {
+            throw new \InvalidArgumentException("The parameter must be a boolean value");
+        }
+
+        $this->italic = $italic;
+
+        return $this;
+    }
+
+    public function isItalic()
+    {
+        return $this->italic;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setUnderline($underline)
+    {
+        if (!in_array($underline, $this->getUnderlines())) {
+            throw new \InvalidArgumentException("The parameter must belong to the Underline parameter list");
+        }
+
+        $this->underline = $underline;
+
+        return $this;
+    }
+
+    public function getUnderline()
+    {
+        return $this->underline;
+    }
+
+    public function getUnderlines()
+    {
+        return [
+            self::UNDERLINE_NONE,
+            self::UNDERLINE_DOUBLE,
+            self::UNDERLINE_DOUBLEACCOUNTING,
+            self::UNDERLINE_SINGLE,
+            self::UNDERLINE_SINGLEACCOUNTING,
+        ];
+    }
 }
--- test/ExcelAnt/Style/FontTest.php
+++ test/ExcelAnt/Style/FontTest.php
@@ -4,8 +4,106 @@ namespace ExcelAnt\Style;
 
 class FontTest extends \PHPUnit_Framework_TestCase
 {
-    public function testTmp()
+    public function testSetAndGetName()
     {
-        $this->markTestIncomplete();
+        $font = new Font();
+        $font->setName('Arial');
+
+        $this->assertEquals('Arial', $font->getName());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetSizeWithWrongParameter()
+    {
+        $font = new Font();
+        $font->setSize('foo');
+    }
+
+    public function testSetAndGetSize()
+    {
+        $font = new Font();
+        $font->setSize(20);
+
+        $this->assertEquals(20, $font->getSize());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetBoldWithWrongParameter()
+    {
+        $font = new Font();
+        $font->setBold('foo');
+    }
+
+    public function testEnableAndDisableBold()
+    {
+        $font = new Font();
+        $font->setBold(true);
+
+        $this->assertTrue($font->isBold());
+
+        $font->setBold(false);
+
+        $this->assertFalse($font->isBold());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetItalicWithWrongParameter()
+    {
+        $font = new Font();
+        $font->setItalic('foo');
+    }
+
+    public function testEnableAndDisableItalic()
+    {
+        $font = new Font();
+        $font->setItalic(true);
+
+        $this->assertTrue($font->isItalic());
+
+        $font->setItalic(false);
+
+        $this->assertFalse($font->isItalic());
+    }
+
+    public function testSetAndGetColor()
+    {
+        $font = new Font();
+        $font->setColor('ff0000');
+
+        $this->assertEquals('ff0000', $font->getColor());
+    }
+
+    /**
+     * @dataProvider getWrongParameters
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetUnderlineWithWongParameter()
+    {
+        $font = new Font();
+        $font->setUnderline('foo');
+    }
+
+    public function testSetAndGetUnderline()
+    {
+        $font = new Font();
+        $font->setUnderline(Font::UNDERLINE_DOUBLE);
+
+        $this->assertEquals(Font::UNDERLINE_DOUBLE, $font->getUnderline());
+    }
+
+    public function getWrongParameters()
+    {
+        return [
+            ['foo'],
+            [''],
+            [null],
+            ['@&'],
+        ];
     }
 }
