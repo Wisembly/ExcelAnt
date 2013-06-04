61a6f78da76745f4b05840fbb4623892bb764bfe
Rémy GAZELOT <r.gazelot@gmail.com>
2013-06-04 14:36:04 +0200

    Create StyleWorker

---
+++ lib/ExcelAnt/PhpExcel/Writer/StyleWorker.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace ExcelAnt\PhpExcel\Writer;
+
+use PHPExcel_Worksheet;
+
+use ExcelAnt\Coordinate\Coordinate;
+use ExcelAnt\Collections\StyleCollection;
+use ExcelAnt\Style\Fill;
+use ExcelAnt\Style\Font;
+use ExcelAnt\Style\Borders;
+use ExcelAnt\Style\Alignment;
+
+class StyleWorker
+{
+    public function applyStyles(PHPExcel_Worksheet $phpExcelWorksheet, Coordinate $coordinate, StyleCollection $styleCollection)
+    {
+        $styles = [];
+
+        foreach ($styleCollection as $style) {
+
+            switch (get_class($style)) {
+
+                case 'ExcelAnt\Style\Fill':
+                    $styles['fill'] = $this->fillManager($style);
+                    break;
+
+                case 'ExcelAnt\Style\Font':
+                    $styles['font'] = $this->fontManager($style);
+                    break;
+
+                case 'ExcelAnt\Style\Borders':
+                    $styles['borders'] = $this->bordersManager($style);
+                    break;
+
+                case 'ExcelAnt\Style\Alignment':
+                    $styles['alignment'] = $this->alignmentManager($style);
+                    break;
+            }
+        }
+
+        $phpExcelWorksheet
+            ->getStyleByColumnAndRow($coordinate->getXAxis() - 1, $coordinate->getYAxis())
+            ->applyFromArray($styles);
+
+        return $phpExcelWorksheet;
+    }
+
+    private function fillManager(Fill $fill)
+    {
+        return [
+            'color' => ['rgb' => $fill->getColor()],
+        ];
+    }
+
+    private function fontManager(Font $font)
+    {
+        return [
+            'name'      => $font->getName(),
+            'size'      => $font->getSize(),
+            'bold'      => $font->isBold(),
+            'italic'    => $font->isItalic(),
+            'color'     => ['rgb' => $font->getColor()],
+            'underline' => $font->getUnderline(),
+        ];
+    }
+
+    private function alignmentManager(Alignment $alignemnt)
+    {
+        return [
+            'horizontal' => $alignemnt->getHorizontal(),
+            'vertical'   => $alignemnt->getVertical(),
+        ];
+    }
+
+    private function bordersManager(Borders $borders)
+    {
+        $bordersToReturn = [];
+
+        if (null !== $borders->getTop()) {
+            $bordersToReturn['top'] = [
+                'color' => $borders->getTop()->getcolor(),
+                'type'  => $borders->getTop()->getType(),
+            ];
+        }
+
+        if (null !== $borders->getBottom()) {
+            $bordersToReturn['bottom'] = [
+                'color' => $borders->getBottom()->getcolor(),
+                'type'  => $borders->getBottom()->getType(),
+            ];
+        }
+
+        if (null !== $borders->getLeft()) {
+            $bordersToReturn['left'] = [
+                'color' => $borders->getLeft()->getcolor(),
+                'type'  => $borders->getLeft()->getType(),
+            ];
+        }
+
+        if (null !== $borders->getRight()) {
+            $bordersToReturn['right'] = [
+                'color' => $borders->getRight()->getcolor(),
+                'type'  => $borders->getRight()->getType(),
+            ];
+        }
+
+        return $bordersToReturn;
+    }
+}
---
+++ test/ExcelAnt/PhpExcel/Writer/StyleWorkerTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ExcelAnt\PhpExcel\Writer;
+
+use ExcelAnt\Coordinate\Coordinate;
+use ExcelAnt\Collections\StyleCollection;
+use ExcelAnt\Style\Fill;
+use ExcelAnt\Style\Font;
+use ExcelAnt\Style\Format;
+use ExcelAnt\Style\Borders;
+use ExcelAnt\Style\Border;
+use ExcelAnt\Style\Alignment;
+
+class StyleWorkerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testApplyStyleOnCell()
+    {
+        $localStyleStorage = [];
+
+        $phpExcelWorksheet = $this->getPhpExcelWorksheetMock();
+        $phpExcelStyle = $this->getPhpExcelStyleMock();
+
+        $phpExcelStyle->expects($this->any())
+            ->method('applyFromArray')
+            ->will($this->returnCallback(function($styles) use (&$localStyleStorage) {
+                $localStyleStorage = $styles;
+            }));
+
+        $phpExcelWorksheet->expects($this->any())
+            ->method('getStyleByColumnAndRow')
+            ->will($this->returnValue($phpExcelStyle));
+
+        $styleCollection = new StyleCollection([
+            (new Fill())->setColor('000000'),
+            (new Font())->setName('Verdana'),
+            (new Format())->setFormat(Format::TYPE_BOOL),
+            (new Borders())->setTop((new Border())->setColor('ff0000')),
+            (new Alignment())->setVertical(Alignment::VERTICAL_CENTER),
+        ]);
+
+        $expected = [
+            'fill' => ['color' => ['rgb' => '000000']],
+            'font' => [
+                'name'      => 'Verdana',
+                'size'      => 11,
+                'bold'      => false,
+                'italic'    => false,
+                'color'     => ['rgb' => '000000'],
+                'underline' => Font::UNDERLINE_NONE,
+            ],
+            'alignment'     => [
+                'horizontal' => 'general',
+                'vertical'   => 'center',
+            ],
+            'borders' => [
+                'top' => [
+                    'color' => 'ff0000',
+                    'type'  => Border::BORDER_NONE,
+                ],
+            ],
+        ];
+
+        $styleWorker = (new StyleWorker())->applyStyles($phpExcelWorksheet, new Coordinate(1, 1), $styleCollection);
+
+        $this->assertEquals($expected, $localStyleStorage);
+    }
+
+    /**
+     * Mock PHPExcel_Worksheet
+     * @return Mock
+     */
+    private function getPhpExcelWorksheetMock()
+    {
+        return $this->getMockBuilder('PHPExcel_Worksheet')->disableOriginalConstructor()->getMock();
+    }
+
+    /**
+     * Mock PHPExcel_Style
+     * @return Mock
+     */
+    private function getPhpExcelStyleMock()
+    {
+        return $this->getMockBuilder('PHPExcel_Style')->disableOriginalConstructor()->getMock();
+    }
+}
