6fcfe3bdafcf7d3a9d6260592d268cd9e478a6a2
Dan Morphis <dan.m@aptalaska.com>
2013-09-30 14:32:48 -0800

    Add support for exporting to Excel 2007 format

---
+++ lib/ExcelAnt/Adapter/PhpExcel/Writer/PhpExcelWriter/Excel2007.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ExcelAnt\Adapter\PhpExcel\Writer\PhpExcelWriter;
+
+use \PHPExcel;
+use \PHPExcel_Writer_Excel2007;
+
+class Excel2007 implements PhpExcelWriterInterface
+{
+    private $path;
+
+    /**
+     * @param string $path The location where the file will be saved
+     */
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function save(PHPExcel $phpExcel)
+    {
+        $writer = (new \PHPExcel_Writer_Excel2007($phpExcel))
+            ->save($this->path);
+    }
+}
